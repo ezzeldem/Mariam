a57a37c9a630f97567ce8c0c55c878586b178279
Ezz Elden <ezzelden06@gmail.com>
2022-02-23 22:37:30 +0200

    new

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,15 @@
-$(document).ready(function(){
+jQuery(document).ready(function($){
+
+  $('.opne_buttom').click(function (e) { 
+    e.preventDefault();
+    $('.nav_link').addClass('opne_menu');
+    $('.over_lay').show();
+  });
+  $('.time_icon, .over_lay').click(function (e) { 
+    e.preventDefault();
+    $('.nav_link').removeClass('opne_menu');
+    $('.over_lay').hide();
+  });
 
   
 });
